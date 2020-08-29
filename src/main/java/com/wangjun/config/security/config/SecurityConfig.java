778b9ea07bb14e9cff068fd8f8b4f0b37adc2526
wangjun <aijun1107>
2020-08-29 22:12:11 +0800

    接口开发

--- src/main/java/com/wangjun/config/security/config/SecurityConfig.java
+++ src/main/java/com/wangjun/config/security/config/SecurityConfig.java
@@ -1,13 +1,9 @@
 package com.wangjun.config.security.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * 标题
@@ -19,13 +15,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/test/**", "/login/**", "/logout/**")
+                .antMatchers("/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
