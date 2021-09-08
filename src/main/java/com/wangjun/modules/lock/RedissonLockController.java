16cd613841bd52addd1bdf0c8cf5cdcb2019cfbe
wangjun <1jun.wang@hosjoy.com>
2021-09-08 16:12:47 +0800

    +++

--- src/main/java/com/wangjun/modules/lock/RedissonLockController.java
+++ src/main/java/com/wangjun/modules/lock/RedissonLockController.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.TimeUnit;
 
 /**
  * <标题>
